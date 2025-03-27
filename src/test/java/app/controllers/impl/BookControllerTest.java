273c757e32979996295aec1e7b72bfb3975d44dd
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2025-03-27 12:17:15 +0100

    virker nu

---
+++ src/test/java/app/controllers/impl/BookControllerTest.java
@@ -0,0 +1,49 @@
+package app.controllers.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void readAllFromUser() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void saveBooksToDatabase() {
+    }
+
+    @Test
+    void populate() {
+    }
+}
